15ef21ad26b6cce038fd695f3f947ff3f96b5b8b
Ricki Moore <mrricki.m.usmc@gmail.com>
2020-04-05 12:04:56 +0200

    updated chat

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -1,61 +1,81 @@
 <template>
-  <div class="window">
-    <chatHeader v-bind:socket="socket"/>
-    <div id="content" class="chat-content">
-      <div class="chat-window">
-        <p class="text" v-bind:class="{'center': message.isIntro || message.isOutro}" v-for="(message, index) in board" v-bind:key="index">
-          <template v-if="message.isIntro && message.name !== user.name">
-            <span>** {{ message.name }} has entered the chat **</span>
-          </template>
-          <template v-if="message.isOutro && message.name !== user.name">
-            <span>** {{ message.name }} has left the chat **</span>
-          </template>
-          <template v-if="!message.isIntro">
-            <span><span v-bind:class="{'highlight': user.name === message.sender}">{{ message.sender }}</span>: {{ message.data }}</span>
-          </template>
-        </p>
-      </div>
-    </div>
-    <div class="input-group animated" v-bind:class="{'shake': isError}">
-      <input
-        v-model="message"
-        v-on:keyup.enter="submit"
-        placeholder="Say something..."
-        type="text"
-      />
-      <button v-on:click="submit">Submit</button>
+  <div class="chat">
+    <div class="screen" v-bind:class="[screen]">
+      <chatHeader v-bind:socket="socket" v-bind:screen="screen" @close="closeChat" @open="openChat" />
+      <template v-if="screen === 'FULLSCREEN'">
+        <div id="content" class="chat-content">
+          <div class="chat-window">
+            <p
+                    class="text"
+                    v-bind:class="{ center: message.isIntro || message.isOutro }"
+                    v-for="(message, index) in board"
+                    v-bind:key="index"
+            >
+              <template v-if="message.isIntro && message.name !== user.name">
+                <span>** {{ message.name }} has entered the chat **</span>
+              </template>
+              <template v-if="message.isOutro && message.name !== user.name">
+                <span>** {{ message.name }} has left the chat **</span>
+              </template>
+              <template v-if="!message.isIntro">
+              <span
+              ><span
+                      v-bind:class="{ highlight: user.name === message.sender }"
+              >{{ message.sender }}</span
+              >: {{ message.data }}</span
+              >
+              </template>
+            </p>
+          </div>
+        </div>
+        <div class="input-group animated" v-bind:class="{ shake: isError }">
+          <input
+                  v-model="message"
+                  v-on:keyup.enter="submit"
+                  placeholder="Say something..."
+                  type="text"
+          />
+          <button v-on:click="submit">Submit</button>
+        </div>
+      </template>
     </div>
   </div>
 </template>
 
 <script>
-import io from "socket.io-client";
-import chatHeader from './chat-header';
+import chatHeader from "./chat-header";
+
+const FULLSCREEN = 'FULLSCREEN';
+const HIDESCREEN = 'HIDESCREEN';
+
 export default {
   name: "ChatBox",
   components: {
     chatHeader
   },
   props: {
-    user: Object
+    user: Object,
+    player: Object,
+    socket: Object,
   },
   data() {
     return {
-      socket: {},
       board: [],
       message: "",
-      isError: false
+      bufferTime: null,
+      isError: false,
+      screen: FULLSCREEN
     };
   },
   methods: {
-    toggleAnimation: function () {
+    toggleAnimation: function() {
       this.isError = true;
       setTimeout(() => {
         this.isError = false;
       }, 1000);
     },
     submit: function() {
-      if(!this.message){
+      if (!this.message) {
         this.toggleAnimation();
         return;
       }
@@ -92,12 +112,14 @@ export default {
       setTimeout(() => {
         const objDiv = document.getElementById("content");
         objDiv.scrollTop = objDiv.scrollHeight;
-      }, 500)
+      }, 500);
+    },
+    openChat: function () {
+      this.screen = FULLSCREEN;
+    },
+    closeChat: function () {
+      this.screen = HIDESCREEN;
     }
-  },
-  created() {
-    this.socket = io(process.env.VUE_APP_SOCKET);
-    window.addEventListener("beforeunload", this.beforePageDestroyed);
   },
   mounted() {
     this.socket.on("introduce", data => {
@@ -110,24 +132,47 @@ export default {
     this.socket.on("relay", data => {
       this.logMessage(data, "relay");
     });
+
+    this.socket.on("bufferMusic", time => (this.bufferTime = time));
+
+    if (this.player) {
+      // this.player.play();
+    }
   }
 };
 </script>
 
 <style scoped>
-.window {
-  width: 100%;
+.chat {
+  position: absolute;
+  right: 0;
+  top: 0;
+  height: 100%;
+  width: 300px;
   display: flex;
   flex-direction: column;
-  align-items: center;
+  justify-content: flex-end;
+}
+.screen{
+  width: 100%;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+}
+.FULLSCREEN{
+  height: 100%;
+}
+.HIDESCREEN {
+  height: 50px;
+  border: 1px solid black;
 }
 .chat-content {
-  width: 50%;
+  width: 100%;
+  flex: 2;
   display: flex;
   overflow: scroll;
   border: 1px solid #d0d0d0;
   border-radius: 15px;
-  height: 400px;
 }
 .chat-window {
   width: 100%;
@@ -135,7 +180,6 @@ export default {
   padding: 10px;
 }
 .input-group {
-  width: 50%;
   display: flex;
   flex-direction: row;
   flex-wrap: nowrap;
@@ -159,16 +203,34 @@ export default {
   border: none;
   border-radius: 5px;
 }
-.highlight{
+.highlight {
   color: #e77471;
 }
-  .text{
-    margin: 5px;
-    text-align: left;
-  }
-  .center {
-    text-align: center;
-  }
+.text {
+  margin: 5px;
+  text-align: left;
+}
+.center {
+  text-align: center;
+}
+.count_btn {
+  width: 20px;
+  height: 20px;
+  background-color: grey;
+  border-radius: 50%;
+  text-align: center;
+  color: white;
+  outline: none;
+  border: none;
+  position: absolute;
+  right: 5px;
+  top: 0;
+  font-size: 12px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
 
 @media (max-width: 600px) {
   .chat-content {
